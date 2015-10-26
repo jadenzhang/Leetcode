b67bc252f07061fbddf1bce449664824102b13f1
jadenzhang <jiadongjaden.zhang@gmail.com>
2015-10-25 21:42:23 -0700

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -5,14 +5,7 @@ public:
         vector<int> sol;
         vector<bool> used(nums.size(),false);
         if (!nums.size()) return res;
-        for (int i=0;i<nums.size();i++){
-            used[i]=true;
-            sol.push_back(nums[i]);
-            findper(nums,res,sol,used,1);
-            sol.pop_back();
-            used[i]=false;
-        }
-        
+        findper(nums,res,sol,used,0);
         return res;
     }
     
